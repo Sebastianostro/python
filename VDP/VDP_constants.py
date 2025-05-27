d3a38f8ca9a2cd60ec18db8512187f00b6aabe90
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2025-05-27 13:51:30 +0200

    Various updates to the modules to include a separate file for constants

---
+++ VDP/VDP_constants.py
@@ -0,0 +1,12 @@
+# Python module for VDP constants
+
+# fine structure constant
+alpha = 1/137
+# Mass of the electron in MeV
+m_e = 0.511
+# Radius of the electron in 1/fm
+r_e = 1.44/m_e
+# Radius of the electron in cm
+r_e = r_e * 1e-13
+# Avogadro's number
+N_A = 6.022*10**23
--- VDP/VDP_functions.py
+++ VDP/VDP_functions.py
@@ -1,4 +1,8 @@
-
+# This file contains various functions related to particle physics calculations, particularly focusing on the Bethe-Bloch formula and related concepts.
+import numpy as np
+import math
+# Constants
+import VDP_constants as const
 # Coulomb correction function for electron/positron
 def f_coul(Z):
     a = Z/137
@@ -8,7 +12,7 @@ def f_coul(Z):
 # Separate helper function for the common factor of the Bethe-Bloch formula
 def bethe_bloch_common_factor():
     # Calculate the common factor
-    K = 2 * np.pi * N_A * r_e**2 * m_e
+    K = 2 * np.pi * const.N_A * const.r_e**2 * const.m_e
     return K
 
 # Bethe-Bloch formula for the energy loss
@@ -16,32 +20,32 @@ def bethe_bloch(Z_det, A_det, rho_det, Z_projectile, beta_projectile, W_max, I_p
     # Calculate beta_gamma
     beta_gamma = beta_projectile / np.sqrt(1 - beta_projectile**2)
     # Calculate common factor
-    K = 2 * np.pi * N_A * r_e**2 * m_e
-    dE = K * Z_det / A_det * rho_det * Z_projectile**2 / beta_projectile**2 * (np.log(2 * m_e * beta_gamma**2 * W_max / I_potential**2) - 2 * beta_projectile**2 - density_correction - 2 * coulomb_correction/Z_det)
+    K = 2 * np.pi * const.N_A * const.r_e**2 * const.m_e
+    dE = K * Z_det / A_det * rho_det * Z_projectile**2 / beta_projectile**2 * (np.log(2 * const.m_e * beta_gamma**2 * W_max / I_potential**2) - 2 * beta_projectile**2 - density_correction - 2 * coulomb_correction/Z_det)
     return dE
 
 # number of atoms in 1 cm^3
 def rho_to_N(rho, A):
     # Calculate number of atoms per cm^3
-    N = rho * N_A / A
+    N = rho * const.N_A / A
     return N
 # Sollte 2,7*10^19 sein
 
 # Function for the radiative correction
-def phi_rad(Z, r_e, alpha, E0, m_e, shield_ind):
+def phi_rad(Z, E0, shield_ind):
     # Calculate common factor
-    cf = 4 * Z**2 * r_e**2 * alpha
+    cf = 4 * Z**2 * const.r_e**2 * const.alpha
     # Calculate the radiative correction
     if shield_ind == 0:
-        phi = cf * (np.log(2 * E0 / m_e) - 1/3 - f_coul(Z))
+        phi = cf * (np.log(2 * E0 / const.m_e) - 1/3 - f_coul(Z))
     elif shield_ind == 1:
         phi = cf * (np.log(183*Z**(-1/3)) + 1/18 - f_coul(Z))
     return phi
 
 # Function for the energy loss
-def dE_dx(E0, Z, A, rho, r_e, alpha, m_e, shield_ind):
+def dE_dx(E0, Z, A, rho, shield_ind):
     # Calculate the energy loss
-    dE = rho_to_N(rho, A) * E0 * phi_rad(Z, r_e, alpha, E0, m_e, shield_ind)
+    dE = rho_to_N(rho, A) * E0 * phi_rad(Z, E0, shield_ind)
     return dE
 
 # Approximation of the critical energy for electrons
@@ -67,8 +71,8 @@ def bh_distribution(E, E0, Z, A, material_length):
     return dN 
 
 # Function to calculate the maximum energy loss (or energy transfer)
-def E_max(beta_gamma, gamma, m_e, M):
+def E_max(beta_gamma, gamma, M):
     # Calculate the maximum energy transfer under the assumption of natural units
     # where c = 1 and masses are in MeV/c^2
-    E_max = 2 * m_e * beta_gamma**2 / (1 + 2 * gamma * (m_e/M) + (m_e/M)**2)
+    E_max = 2 * const.m_e * beta_gamma**2 / (1 + 2 * gamma * (const.m_e / M) + (const.m_e / M)**2)
     return E_max
