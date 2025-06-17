62047c69185ec2fea0501486936a62249a1b3520
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2025-06-17 15:06:52 +0200

    Various parameter changes and tests.

--- Theo_KT/constants.py
+++ Theo_KT/constants.py
@@ -1,5 +1,7 @@
 # constants.py
 
+# general factor to connect MeV to fm
+hquerfm = 197.327  # MeV*fm
 # Default parameter values (can be updated or fitted later)
 B = 0.145  # GeV^4 (example from MIT paper)
 Z0 = 1.84  # dimensionless
