2f52bd9ce6629160f8cda10ed6fac99653a09e6d
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2025-07-01 14:16:12 +0200

    Removal of delta_em from function "energy_terms" in bagmodel.py

--- Theo_KT/bagmodel.py
+++ Theo_KT/bagmodel.py
@@ -8,7 +8,8 @@ def energy_terms(R, n, ns, m=m_light, ms=m_strange, S=0, I=0):
     kinetic = n * np.sqrt(m**2 + (x / R)**2) + ns * np.sqrt(ms**2 + (x / R)**2)
     bag_energy = (B**4) * (4 / 3) * np.pi * R**3 - Z0 / R
     delta_em = (4 / 3) * alpha_c / R * (n * (n - 6) + S * (S + 1) + 3 * I * (I + 1))
-    return kinetic + bag_energy + delta_em
+    return kinetic + bag_energy 
+#+ delta_em
 
 def minimize_energy(n, ns, S, I, m=m_light, ms=m_strange):
     result = sp.optimize.minimize_scalar(lambda R: energy_terms(R, n, ns, m, ms, S, I), bounds=(0,10), method='bounded')
